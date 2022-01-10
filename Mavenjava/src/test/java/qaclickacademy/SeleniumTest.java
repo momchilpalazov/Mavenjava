478555dd5e2a7f4a9ed8e365c1f51269c92bbaa9
Palazov <Palazov@DESKTOP-J833GHP>
2022-01-10 16:50:04 +0100

    Maven Project Create with pom.xml file importnace  surefirePlugin ,Maven profiling
    Maven project

---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAUtomtaion()
+	{
+		System.out.println("BrowserAUtomtaio");
+	}
+	
+	@Test
+	public void elemtsUi()
+	{
+		
+		System.out.println("elemtsUi");
+	}
+	
+
+}
